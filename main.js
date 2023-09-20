9ae817d91483a5e91e5641337df31f9e5903c711
Kheldaan <begards@laposte.net>
2023-09-20 15:17:05 +0200

    HTML CSS JS trains

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
-import fs from "./fs";
-console.log(fs);
+//import fs from "./fs";
+//console.log(fs);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -27,28 +27,52 @@ form.addEventListener("submit", (e) => {
 
 
 function selectionCompteur() {
+    const selectElement = document.getElementById("prestataires");
 
-let selectElement = document.getElementById("prestataires");
+    let sncfCompteur = 0;
+    let thalysCompteur = 0;
+    let ouiGoCompteur = 0;
+    let eurostarCompteur = 0;
+    let tgvCompteur = 0;
+    let trenitaliaCompteur = 0;
 
-let sncfCompteur = 0;
-let ouiGoCompteur = 0;
-let airFranceCompteur = 0;
+    for (let i = 0; i < selectElement.options.length; i++) {
+        let option = selectElement.options[i];
 
-for (let i = 0; i < selectElement.options.lenght; i++) {
-    let option = selectElement.options[i];
-
-if (option.selected) {
-    if (option.value === "sncf") {
-        sncfCompteur++;
-    } else if (option.value === "ouigo") {
-        ouiGoCompteur++;
-    } else if (option.value === "airfrance") {
-        airFranceCompteur++;
+        if (option.selected) {
+            if (option.value === "sncf") {
+                sncfCompteur++;
+            } else if (option.value === "thalys") {
+                thalysCompteur++;
+            } else if (option.value === "ouigo") {
+                ouiGoCompteur++;
+            } else if (option.value === "eurostar") {
+                eurostarCompteur++;
+            } else if (option.value === "tgv") {
+                tgvCompteur++;
+            } else if (option.value === "trenitalia") {
+                trenitaliaCompteur++;
+            }
+        }
     }
-}
+
+    return {
+        sncfCompteur: sncfCompteur,
+        thalysCompteur: thalysCompteur,
+        ouiGoCompteur: ouiGoCompteur,
+        eurostarCompteur: eurostarCompteur,
+        tgvCompteur: tgvCompteur,
+        trenitaliaCompteur: trenitaliaCompteur
+    };
 }
 
-console.log(sncfCompteur, ouiGoCompteur, airFranceCompteur);
-
-}
-
+const selectElement = document.getElementById("prestataires");
+selectElement.addEventListener("change", function () {
+    const compteur = selectionCompteur();
+    console.log("SNCF choisi " + compteur.sncfCompteur + " fois");
+    console.log("THALYS choisi " + compteur.thalysCompteur + " fois");
+    console.log("OUIGO choisi " + compteur.ouiGoCompteur + " fois");
+    console.log("Eurostar choisi " + compteur.eurostarCompteur + " fois");
+    console.log("TGV choisi " + compteur.tgvCompteur + " fois");
+    console.log("Trenitalia choisi " + compteur.trenitaliaCompteur + " fois");
+});
